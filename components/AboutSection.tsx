4b0cfd28c13fa5337983e0c31bad7ce4845a9d79
sami23ibrahim <sami23ibrahim@gmail.com>
2023-09-27 22:03:38 -0400

    added new about me

--- components/AboutSection.tsx
+++ components/AboutSection.tsx
@@ -1,138 +1,4 @@
 
-// "use client" // this is a client component ggg
-// import React from "react"
-// import Image from "next/image"
-// import { Link } from "react-scroll/modules"
-// import { HiArrowDown } from "react-icons/hi"
-
-
-
-// const skills = [
-//   { skill: "HTML" },
-//   { skill: "CSS" },
-//   { skill: "JavaScript" },
-//   { skill: "TypeScript" },
-//   { skill: "Python" },
-//   { skill: "React" },
-//   { skill: "Next.js" },
-//   { skill: "Tailwind CSS" },
-//   { skill: "Git" },
-//   { skill: "GitHub" },
-//   { skill: "Java" },
-//   { skill: "C#" },
-//   { skill: "Unity" },
-//   { skill: "Davinci Resolve" },
-//   { skill: "C++" },
-//   { skill: "SQL" },
-// ];
-
-// const AboutSection = () => {
-//   return (
-//     <section id="about">
-//      <div className="my-12 pb-8 md:pt-11 md:pb-14 ">
-//      {/*my-12 pb-12 md:pt-16 md:pb-48 */}
-//         <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-1 md:p-1 md:flex-row md:text-left">
-         
-         
-//           <div className="md:w-1/2">
-//  {/* Adding Education Section Here */}
-//  <h1 className="text-center text-xl font-bold mb-6 md:text-left">Education</h1>
-//             <div className="flex items-center mb-4 align-bottom mt-9">
-//               <Image
-//                 //src="/ConcordiaLogo.png"
-//                 src="/concordiaLogo.png"
-//                 alt="Concordia University Icon"
-//                 width={80}
-//                 height={50}
-//                 className="mr-6" 
-//               />
-//               <div>
-//                 <h2 className="font-bold text-xl">Bachelor of Computer Science </h2>
-//                 <p className="font-bold">Concordia University, Montreal, QC</p>
-//                 <p>2020 - 2023</p>
-//               </div>
-//             </div>
-//             <div className="flex items-center mt-6 ">
-//               <Image
-//                 src="/tishreen.webp" 
-//                 alt="Tishreen University Icon"
-//                 width={80}
-//                 height={50}
-//                 className="mr-6" 
-//               />
-//               <div>
-//                 <h2 className="font-bold text-xl">Bachelor of Economics, Business Administration and Marketing</h2>
-//                 <p className="font-bold">Tishreen University, Latakia, Syria</p>
-//                 <p>2015 - 2018</p>
-//               </div>
-//             </div>
-//             {/* End of Education Section */}
-
-//           </div>
-//          {/********************************************* */}
-//           <div className="text-center md:w-1/2 md:text-left">
-//             <h1 className="text-2xl font-bold mb-6">My Skills</h1>
-//             <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
-//               {skills.map((item, idx) => {
-//                 return (
-//                   <p
-//                     key={idx}
-//                     className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
-//                   >
-//                     {item.skill}
-//                   </p>
-//                 );
-//               })}
-//             </div>
-//           </div>
-
-
-//         </div>
-//       </div>
-
-//       <div className="flex flex-row items-center text-center justify-center  my-2 ">
-        
-//         <Link
-//           to="projects"
-//           activeClass="active"
-//           spy={true}
-//           smooth={true}
-//           offset={-100}
-//           duration={500}
-//         >
-//          <h1 className="text-center font-bold text-3xl mb-4 cursor-pointer">
-          
-//           Jump to my Projects
-//         </h1>
-      
-//         </Link>
-//       </div>
-
-
-//       <div className="flex flex-row items-center text-center justify-center my-2  mb-10 ">
-        
-//         <Link
-//           to="projects"
-//           activeClass="active"
-//           spy={true}
-//           smooth={true}
-//           offset={-100}
-//           duration={500}
-//         >
-//           <HiArrowDown size={35} className="animate-bounce cursor-pointer" />
-//         </Link>
-//       </div>
-
-
-
-
-//     </section>
-//   );
-// };
-
-// export default AboutSection;
-
-
 import React from "react";
 import Image from "next/image";
 
@@ -164,13 +30,10 @@ const AboutSection = () => {
           <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
         </h1>
 
-        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+        <div className="flex flex-col md:flex-row md:space-x-10 md:p-4 md:text-left">
           <div className="md:w-1/2">
-           
-            
-
- {/* Adding Education Section Here */}
- <h1 className="text-center text-xl font-bold mb-6 md:text-left">Education</h1>
+            {/* Education Section */}
+            <h1 className="text-center text-xl font-bold mb-6 md:text-left">Education</h1>
             <div className="flex items-center mb-4 align-bottom mt-9">
               <Image
                 src="/concordiaLogo.png"
@@ -199,66 +62,46 @@ const AboutSection = () => {
                 <p>2015 - 2018</p>
               </div>
             </div>
-            {/* End of Education Section */}
-
-
-
-
-
-
-  {/********************************************* */}
-          
-          </div>
-         {/********************************************* */}
-          <div className="text-center md:w-1/2 md:text-left">
-            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
-            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
-              {skills.map((item, idx) => {
-                return (
-                  <p
-                    key={idx}
-                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
-                  >
-                    {item.skill}
-                  </p>
-                );
-              })}
+            
+            {/* My Skills Section */}
+            <div className="mt-10">
+              <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+              <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+                {skills.map((item, idx) => {
+                  return (
+                    <p
+                      key={idx}
+                      className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                    >
+                      {item.skill}
+                    </p>
+                  );
+                })}
+              </div>
             </div>
-  {/********************************************* */}
-  {/* <h1 className="text-center text-2xl font-bold mb-6 md:text-left">
-              Get to know me!
-            </h1>
-            <p>
-              Hi, my name is Sami and I am a{" "}
-              <span className="font-bold">{"highly ambitious"}</span>,
-              <span className="font-bold">{" self-motivated"}</span>, and
-              <span className="font-bold">{" driven"}</span> Computer scientist
-              based in Montreal, QC.
+          </div>
+          
+         {/* Personalized Summary Section */}
+<div className="md:w-1/2">
+  
+  <h2 className="text-xl font-bold mb-4">🎓 Educational Journey:</h2>
+  <p className="mb-4">
+    Graduating with distinction, my academic journey in Computer Science has been a blend of learning and practical application. I delved deep into subjects like Data Structures, Algorithms, Artificial Intelligence, Game Development, and Software Engineering Methodologies, equipping myself with a versatile skill set in Java, Python, and JavaScript.
+  </p>
+  <h2 className="text-xl font-bold mb-4">💼 Professional Experience:</h2>
+  <p className="mb-4">
+    My stint as a Junior Software Engineer at Zetane Systems Inc. was a learning curve where I wore multiple hats - from designing and implementing software features to enhancing front-end components and boosting SEO scores to 100%. My role also involved leveraging Google Analytics 4 for event-based tracking, aiding the company in making data-driven decisions.
+  </p>
+ 
+  <h2 className="text-xl font-bold mb-4">🌟 Skills & Attributes:</h2>
+  <p className="mb-4">
+    I'm a self-starter and analytical thinker, with a knack for solving problems and a drive to learn and innovate. My dual background in Computer Science and Economics positions me uniquely, enabling me to bring diverse perspectives to both individual and team environments.
+  </p>
+  <h2 className="text-xl font-bold mb-4">🔗 Connect With Me:</h2>
+  <p>
+    sami23ibrahim@gmail.com
+ 
             </p>
-            <br />
-            <p>
-            In 2023, I earned a Bachelor of Computer Science from Concordia University, 
-            complementing my prior degrees in Economics, Business Administration, and Marketing.
-            </p>
-            <br />
-            <p>
-              I have a wide range of hobbies and passions that keep me busy.
-              From reading, playing sports, traveling, to making YouTube videos,
-              I am always seeking new experiences and love to keep myself
-              engaged and learning new things.
-            </p>
-            <br />
-            <p>
-              I believe that you should{" "}
-              <span className="font-bold text-teal-500">
-                never stop growing
-              </span>{" "}
-              and that&#39;s what I strive to do, I have a passion for
-              technology and a desire to always push the limits of what is
-              possible. I am excited to see where my career takes me and am
-              always open to new opportunities. 🙂
-            </p> */}
-           {/********************************************* */}   
           </div>
         </div>
       </div>
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,205 +1,5 @@
-// "use client" // this is a client component
-// import React from "react"
-// import Image from "next/image"
-// import { Link } from "react-scroll/modules"
-// import { HiArrowDown } from "react-icons/hi"
 
-// const HeroSection = () => {
-//   return (
-//     <section id="home">
-//       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-20 py-16 sm:py-28 md:py-11 md:flex-row md:space-x-4 md:text-left">
-//         <div className="md:mt-2 md:w-1/2">
-//           <Image  
-//             src="/sami2.jpg"
-//             alt=""
-//             width={325}
-//             height={325}
-//             className="rounded-full shadow-2xl"
-//           />
-//         </div>
-//         <div className="md:mt-2 md:w-3/5">
-//           <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m SAMI!</h1>
-//           <p className="text-lg mt-4 mb-6 md:text-2xl">
-//             I&#39;m a{" "}
-//             <span className="font-semibold text-teal-300">
-//               Computer science{" "}
-//             </span>
-//             graduate based in Montreal, QC. Working towards creating software that
-//             makes life easier and more meaningful.
-//           </p>
-//           <Link
-//             to="projects"
-//             className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700 cursor-pointer"
-//             activeClass="active"
-//             spy={true}
-//             smooth={true}
-//             offset={-100}
-//             duration={500}
-//           >
-//            Projects
-
-//           </Link>
-//           <span className="mx-2"></span>
-// <a
-//   href="/Sami_Ibrahim_Resume.pdf"
-//   target="_blank"
-//   rel="noopener noreferrer"
-//   className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
-// >
-//   Resume
-// </a>
-// <span className="mx-2"></span>
-// <a
-//   href="https://www.linkedin.com/in/sami-23-ibrahim/"
-//   target="_blank"
-//   rel="noopener noreferrer"
-//   className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
-// >
-//   LinkedIn
-// </a>
-
-//         </div>
-//       </div>
-
-// <div className="flex flex-row items-center text-center justify-center ">
-        
-//         <Link
-//           to="about"
-//           activeClass="active"
-//           spy={true}
-//           smooth={true}
-//           offset={-100}
-//           duration={500}
-//         >
-//          <h1 className="text-center font-bold text-3xl mb-4 cursor-pointer">
-          
-//           Jump to About Me
-//         </h1>
-      
-//         </Link>
-//       </div>
-
-
-//       <div className="flex flex-row items-center text-center justify-center ">
-        
-//         <Link
-//           to="about"
-//           activeClass="active"
-//           spy={true}
-//           smooth={true}
-//           offset={-100}
-//           duration={500}
-//         >
-//           <HiArrowDown size={35} className="animate-bounce cursor-pointer" />
-//         </Link>
-//       </div>
-//     </section>
-//   )
-// }
-
-// export default HeroSection
-//********************************************************************************************************************** */
-
-
-
-// "use client" // this is a client component
-// import React from "react"
-// import Image from "next/image"
-// import { Link } from "react-scroll/modules"
-// import { HiArrowDown } from "react-icons/hi"
-
-// const HeroSection = () => {
-//   return (
-//     <section id="home">
-//       <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-38 md:flex-row md:space-x-4 md:text-left">
-//         <div className="md:mt-2 md:w-1/2">
-//           <Image  
-//             src="/sami2.jpg"
-//             alt=""
-//             width={325}
-//             height={325}
-//             className="rounded-full shadow-2xl"
-//           />
-//         </div>
-//         <div className="md:mt-2 md:w-3/5">
-//           <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m SAMI!</h1>
-//           <p className="text-lg mt-4 mb-6 md:text-2xl">
-//             I&#39;m a{" "}
-//             <span className="font-semibold text-teal-300">
-//               Computer science{" "}
-//             </span>
-//             graduate based in Montreal, QC. Working towards creating software that
-//             makes life easier and more meaningful.
-//           </p>
-//           <Link
-//             to="projects"
-//             className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700 "
-//             activeClass="active"
-//             spy={true}
-//             smooth={true}
-//             offset={-100}
-//             duration={500}
-//           >
-//            Projects
-
-//           </Link>
-//           <span className="mx-2"></span>
-//           {/* <Link
-//             to="Resume"
-//             className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
-//             activeClass="active"
-//             spy={true}
-//             smooth={true}
-//             offset={-100}
-//             duration={500}
-            
-//           >
-//            Resume
-//           </Link> */}
-// <a
-//   href="/Sami_Ibrahim_Resume.pdf"
-//   target="_blank"
-//   rel="noopener noreferrer"
-//   className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
-// >
-//   Resume
-// </a>
-// <span className="mx-2"></span>
-// <a
-//   href="https://www.linkedin.com/in/sami-23-ibrahim/"
-//   target="_blank"
-//   rel="noopener noreferrer"
-//   className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
-// >
-//   LinkedIn
-// </a>
-
-//         </div>
-//       </div>
-//       <div className="flex flex-row items-center text-center justify-center ">
-//         <Link
-//           to="about"
-//           activeClass="active"
-//           spy={true}
-//           smooth={true}
-//           offset={-100}
-//           duration={500}
-//         >
-//           <HiArrowDown size={35} className="animate-bounce" />
-//         </Link>
-//       </div>
-//     </section>
-//   )
-// }
-
-// export default HeroSection
-
-
-
-
-
-
-"use client" // this is a client component
+"use client" // this is a client component rfrffrfrfrf
 import React from "react"
 import Image from "next/image"
 import { Link as OriginalLink } from "react-scroll"
@@ -210,7 +10,7 @@ const Link = OriginalLink as any; // Type assertion to avoid TypeScript error
 const HeroSection = () => {
   return (
     <section id="home">
-      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-38 md:flex-row md:space-x-4 md:text-left">
+      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-38 md:flex-row md:space-x-4 md:text-left mb-[-66px]">
         <div className="md:mt-2 md:w-1/2">
           <Image  
             src="/sami2.jpg"
@@ -220,15 +20,27 @@ const HeroSection = () => {
             className="rounded-full shadow-2xl"
           />
         </div>
-        <div className="md:mt-2 md:w-3/5">
-          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m SAMI!</h1>
+      
+         <div className="md:mt-2 md:w-3/5">
+  <div className="flex items-center">
+    <span className="inline-block align-middle" style={{ fontSize: '2.8em', marginRight: '0.5em' }}>👋</span>
+    <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-4xl">Hi, I&#39;m SAMI!</h1>
+  </div>
+         
           <p className="text-lg mt-4 mb-6 md:text-2xl">
             I&#39;m a{" "}
             <span className="font-semibold text-teal-300">
               Computer science{" "}
             </span>
-            graduate based in Montreal, QC. Working towards creating software that
-            makes life easier and more meaningful.
+            graduate from Concordia University based in Montreal, Canada, and a proud holder of a Bachelor of Economics with a specialization in 
+            <span className="font-semibold text-teal-300">
+            {" "}Marketing{" "}
+            </span>
+            
+            from Tishreen University. I'm passionate about blending technical prowess with marketing acumen to create impactful software solutions that resonate with users.
+
+             
+
           </p>
           <Link
             to="projects"
